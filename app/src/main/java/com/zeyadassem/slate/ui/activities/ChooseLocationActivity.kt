da033c529dd1691823dc6cdde480bb74af86ca02
Zeyad Assem <zeyadsadaqa@gmail.com>
2019-02-28 09:09:24 +0800

    This commit includes: 1- Adding the API ket to manifest. 2- Adding UI to ChooseLocationActivity.

--- app/src/main/java/com/zeyadassem/slate/ui/activities/ChooseLocationActivity.kt
+++ app/src/main/java/com/zeyadassem/slate/ui/activities/ChooseLocationActivity.kt
@@ -2,12 +2,33 @@ package com.zeyadassem.slate.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.zeyadassem.slate.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
 
-class ChooseLocationActivity : AppCompatActivity() {
+
+
+class ChooseLocationActivity : AppCompatActivity(), OnMapReadyCallback {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_location)
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val defaultLocation = LatLng(3.157268, 101.7096623)
+        googleMap?.addMarker(
+            MarkerOptions().position(defaultLocation)
+                .title("Marker in Malaysia")
+        )
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(defaultLocation))
     }
 }
